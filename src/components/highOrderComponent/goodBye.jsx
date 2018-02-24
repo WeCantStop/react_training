4db0cfb641c9685dd7f8536c212d4c23b4c81eed
Will <pan123033@qq.com>
2018-02-24 17:29:07 +0800

    HOC

---
+++ src/components/highOrderComponent/goodBye.jsx
@@ -0,0 +1,17 @@
+import React, {Component} from 'react'
+import wrapperFunc from './highComponent'
+
+class GoodByeOriginal extends Component {
+    constructor(props) {
+        super(props)
+    }
+
+    render() {
+        return (
+            <div>GoodBye {this.props.username || 'no name'}</div>
+        )
+    }
+
+}
+
+export const GoodBye = wrapperFunc(GoodByeOriginal);
--- webpack.config.base.js
+++ webpack.config.base.js
@@ -35,7 +35,8 @@ module.exports = {
     resolve: {
         extensions: ['.js', '.jsx', '.json', '.scss'],
         alias: {
-            '@': path.join(__dirname, '..', 'src')
+            "@": path.resolve(__dirname, 'src/'),
+            "Components": path.resolve(__dirname, 'src/components/')
         }
     },
     
